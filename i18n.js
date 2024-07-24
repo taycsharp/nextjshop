bbeab10d588af0c53af246acd141e7500ccbda23
klc <klc@192.168.1.19>
2024-07-24 09:35:38 +0700

    i18n add

---
+++ i18n.js
@@ -0,0 +1,16 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import { ni18nConfig } from './ni18n.config'
+
+i18n
+  .use(initReactI18next) // Passes i18n down to react-i18next
+  .init({
+    ...ni18nConfig,
+    lng: 'en', // Default language
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false, // React already does escaping
+    },
+  });
+
+export default i18n;

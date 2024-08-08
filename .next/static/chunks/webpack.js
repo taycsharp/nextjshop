1ed678cdb740a47702557510409b8a9cec0675d7
klc <klc@192.168.1.26>
2024-08-08 09:40:23 +0700

    improve translation & pagination

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2a33ad109e57a2dc"; }
+/******/ 		__webpack_require__.h = function() { return "67104af80ece6297"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
